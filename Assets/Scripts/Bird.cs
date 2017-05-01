22b694dd09ee421cb468c8f84d088243d99682be
Michael Stout <stout@uselessopinions.com>
2017-04-30 23:09:34 -0400

    Bird flies on spacepress

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour 
 {
+	public float speed = 2;
+	public float force = 300;
+
+	private Rigidbody2D rb2d = null;
 
 	// Use this for initialization
 	void Awake () 
 	{
-		
+		rb2d = GetComponent<Rigidbody2D>();
 	}//Awake
-	
+
+	void Start()
+	{
+		rb2d.velocity = Vector2.right * speed;
+	}//Start
+
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rb2d.AddForce(Vector2.up * force);
+		}//if
 	}//Update
 }//
