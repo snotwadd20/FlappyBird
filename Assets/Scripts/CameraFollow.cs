23a73b9dcbbe2727c7dde74498775f3037f24e87
Michael Stout <stout@uselessopinions.com>
2017-04-30 23:12:47 -0400

    Camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour 
+{
+	public Transform target = null;
+
+	void LateUpdate () 
+	{
+		//Follow on X-axis, remain steady on others
+		transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+	}//LateUpdate
+}//
