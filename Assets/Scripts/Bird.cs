df507a636213f8ecd12e2a8e66c7a0069194a5e1
Michael Stout <stout@uselessopinions.com>
2017-04-30 23:05:46 -0400

    Bird object and background

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Awake () 
+	{
+		
+	}//Awake
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}//Update
+}//
